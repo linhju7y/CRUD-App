9f9424c2e441dcca2e7332d9769b7193b2be3646
linhju7y <linhju7y@gmail.com>
2021-01-29 20:42:11 +0700

    2nd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, {Component} from 'react'
+import ProductItem from './ProductItem';
+import AddProduct from './AddProduct';
+
+
+const products = [
+  {
+    name: 'iPad',
+    price: 200
+  },
+  {
+    name: 'iPhone',
+    price: 700
+  },
+
+];
+
+
+localStorage.setItem('products', JSON.stringify(products)) 
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      products: JSON.parse(localStorage.getItem('products'))
+    };
+
+    this.onDelete = this.onDelete.bind(this);
+    this.onAdd = this.onAdd.bind(this);
+    this.onEditSubmit = this.onEditSubmit.bind(this);
+
+  }
+
+  componentWillMount(){
+    const products = this.getProducts();
+    this.setState({products});
+
+  }
+
+  getProducts(){
+    return this.state.products;
+  }
+
+  onDelete(name){
+    const products = this.getProducts();
+
+    const filteredProducts = products.filter(product => {
+      return product.name !== name;
+    });
+
+    this.setState({products: filteredProducts});
+
+  }
+
+  onAdd(name, price){
+    const products = this.getProducts();
+
+    products.push({
+      name,
+      price
+    });
+
+    this.setState({ products });
+  }
+
+  onEditSubmit(name, price, originalName){
+    let products = this.getProducts();
+    
+    products = products.map(product => {
+      if(product.name === originalName) {
+        product.name = name;
+        product.price = price; 
+      }
+
+      return product;
+    });
+
+    this.setState({ products });
+  }
+  
+  
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Products Manager</h1>
+
+      <AddProduct 
+        onAdd={this.onAdd}
+      />
+      
+      {
+        this.state.products.map(product => {
+          return (
+           <ProductItem 
+            key={product.name}
+            { ...product}
+            onDelete={this.onDelete}
+            onEditSubmit={this.onEditSubmit}
+            />
+          )
+        })
+      }
+  
     </div>
   );
+        }
 }
-
 export default App;
